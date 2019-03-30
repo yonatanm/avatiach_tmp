4d7136faaee09a3239c02e8d1bd02d71a63d1cb4
itamarmaman <itamar.maman@gmail.com>
2019-03-30 22:06:54 +0300

    playing with css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="header">HEADER</div>
+        <div class="nav">NAV</div>
+        <div class="main">MAIN</div>
+        <div class="aside">ASIDE</div>
+        <div class="footer">FOOTER</div>
       </div>
     );
   }
