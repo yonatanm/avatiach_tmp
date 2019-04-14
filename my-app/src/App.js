38d124aa9aca70f932e9e74a79acaa1b6094e502
itamarmaman <itamar.maman@gmail.com>
2019-04-14 22:49:28 +0300

    created home component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import search from './magnifier-tool.png';
 import './App.scss';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatiach: {id: 12, songName: "Hana Zelda", worngLine: "blah", correctLine:"boo"},
+      home: !true
+    };    
+  }
+
   render() {
     return (
       <div className="App">
-        <div class="header">HEADER</div>
-        <div class="nav">NAV</div>
-        <div class="main">MAIN</div>
-        <div class="aside">ASIDE</div>
-        <div class="footer">FOOTER</div>
+        <div className="header">HEADER</div>
+        <div className="nav">NAV<Search></Search></div>
+        <Main avatiach={this.state.avatiach} home={this.state.home}>
+        </Main>
+        <div className="aside">ASIDE</div>
+        <div className="footer">FOOTER</div>
       </div>
     );
   }
+}
+class Main extends Component {
+  render() {
+    if (this.props.home === true) {
+      return (
+      <Home></Home>
+      )
+    }
+    if (this.props.avatiach) {
+      return (
+      <Avatiach avatiach={this.props.avatiach}></Avatiach>
+      )
+    }
+    return (<div className="main">MAIN</div>)
+  }
+}
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: false,
+    };    
+  }
+  
+  handdleClick() {
+    this.setState({search: !this.state.search})
+  }
+  
+  render() {
+    var serachInput
+    if (this.state.search)  {
+      serachInput = <div><input className = "search-input"></input><button>search</button></div>
+    }
+    return (
+      <div>
+        <img onClick = {() => this.handdleClick()} src = {search} className = "search-img"></img>
+        {serachInput}
+      </div>
+    );    
+  }
+}
+function Home() {
+  return (
+  <h1>Welcome to Avatiach</h1>
+  )
+}
+function Avatiach(props) {
+  console.log(props)
+  return (
+  <div>
+    <h2>{props.avatiach.songName}</h2>
+    <h3>{props.avatiach.worngLine}</h3>
+    <h3>{props.avatiach.correctLine}</h3>
+  </div>
+  )
 }
 export default App;
